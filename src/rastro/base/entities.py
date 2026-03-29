920d00cd5583c1b7a130bdb65432734582036cdf
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-03-30 00:09:52 +0100

    vai

--- src/rastro/base/entities.py
+++ src/rastro/base/entities.py
@@ -1,12 +1,19 @@
 from rastro.base.value_objects import ValueObject
 
 
-class Id(ValueObject[str]):
-    def __post_init__(self) -> None:
-        pass
+class Id(ValueObject[int]):
+    def validate(self) -> None:
+        if not self.value >= 1:
+            raise ValueError()
 
 
-# abc = Id()
-# abc.cop
-print(Id(1) == 
-      Id(2))
+id1 = Id(1)
+id2 = Id(2)
+id1_2 = Id(1)
+
+idBREAKS = Id(-1)
+
+print(id1)
+print(id2)
+print(id1 == id2)
+print(id1 == id1_2)
--- src/rastro/base/value_objects.py
+++ src/rastro/base/value_objects.py
@@ -1,5 +1,5 @@
-from abc import ABC
-from dataclasses import dataclass, field
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from typing import Generic, TypeVar
 
 T = TypeVar("T")
@@ -7,4 +7,10 @@ T = TypeVar("T")
 
 @dataclass(frozen=True)
 class ValueObject(ABC, Generic[T]):
-    value: T = field()
+    value: T
+
+    @abstractmethod
+    def validate(self) -> None: ...
+
+    def __post_init__(self) -> None:
+        self.validate()
